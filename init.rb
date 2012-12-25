6ac7ddcf8c5b1418370bada9f1cc6e8a828abe5c
serggl <sergey.glukhov@gmail.com>
2012-12-25 14:15:06 +0400

    change path and description

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
 require_dependency 'inline_image_hook/hooks'
 
-Redmine::Plugin.register :redmine_inline_note_images do
+Redmine::Plugin.register :redmine_inline_note_images_plugin do
   name 'Inline Note Images plugin'
   author 'AnjLab'
   description 'Inline note images for Redmine issues'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/anjlab/redmine_inline_note_images_plugin'
   author_url 'http://anjlab.com'
 end
