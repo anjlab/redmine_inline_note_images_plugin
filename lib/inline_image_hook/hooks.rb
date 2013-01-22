9985323abb44f24fce0cbcf058b4f751f0dd9afe
And <logical_and@lavabit.com>
2013-01-22 11:33:34 +0200

    Move styles to stylesheet file and stylizing attachments

--- lib/inline_image_hook/hooks.rb
+++ lib/inline_image_hook/hooks.rb
@@ -7,4 +7,12 @@ class Hooks < Redmine::Hook::ViewListener
   render_on :view_issues_history_journal_bottom,
             :partial => 'hooks/inline_image_hook/inline_images_paste'#,
             #locals: context
+
+  def view_layouts_base_html_head(context={})
+    if context[:controller] && (context[:controller].is_a?(IssuesController))
+      return stylesheet_link_tag("inline_images.css", :plugin => "redmine_inline_note_images_plugin", :media => "screen")
+    else
+      return ''
+    end
+  end
 end
